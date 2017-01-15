9bc57bf74d105de2a763822b1b267a19cdc2fa0f
Kerby Ferris <kerbyferris@gmail.com>
2017-01-14 17:48:02 -0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,61 @@
+(defproject treeact "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :min-lein-version "2.7.1"
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [org.clojure/core.async "0.2.391"
+                  :exclusions [org.clojure/tools.reader]]
+                 [reagent "0.6.0"]
+                 [cljs-bach "0.2.0"]
+                 [squelch "0.1.3"]
+                 ]
+
+  :plugins [[lein-figwheel "0.5.8"]
+            [lein-cljsbuild "1.1.4" :exclusions [[org.clojure/clojure]]]]
+
+  :source-paths ["src"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+
+                :figwheel {:on-jsload "treeact.core/on-js-reload"}
+
+                :compiler {:main treeact.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/treeact.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true
+                           :preloads [devtools.preload]}}
+               ;; This next build is an compressed minified build for
+               ;; production. You can build this with:
+               ;; lein cljsbuild once min
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/treeact.js"
+                           :main treeact.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {:css-dirs ["resources/public/css"] ;; watch and update CSS
+             }
+
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.8.2"]
+                                  [figwheel-sidecar "0.5.8"]
+                                  [com.cemerick/piggieback "0.2.1"]]
+                   ;; need to add dev source path here to get user.clj loaded
+                   :source-paths ["src" "dev"]
+                   ;; for CIDER
+                   ;; :plugins [[cider/cider-nrepl "0.12.0"]]
+                   :repl-options {; for nREPL dev you really need to limit output
+                                  :init (set! *print-length* 50)
+                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+
+)
---
+++ src/treeact/core.cljs
@@ -0,0 +1,142 @@
+(ns treeact.core
+  (:require [reagent.core :as r :refer [atom]]
+            [cljs-bach.synthesis :as audio :refer [connect->]]
+            [squelch.core :as s]))
+
+(enable-console-print!)
+
+(defonce context (audio/audio-context))
+
+(defonce app-state (atom {:chakra 5, :color "blue"}))
+
+(defn set-level! [level]
+  (swap! app-state assoc :chakra (:chakra level))
+  (swap! app-state assoc :color (:color level)))
+
+(defn ping [freq]
+  (connect->
+    (audio/sine freq)         ; Try a sawtooth wave.
+    (audio/percussive 0.3 1.6) ; Try varying the attack and decay.
+    (audio/gain 0.4)))          ; Try a bigger gain.
+
+(defn chakra-1 [] 
+  [:div
+    (-> (ping 174)
+        (audio/connect-> audio/destination)
+        (audio/run-with context (audio/current-time context) 1.0))
+   ]
+  )
+(defn chakra-2 []
+  [:div
+    (-> (ping 285)
+        (audio/connect-> audio/destination)
+        (audio/run-with context (audio/current-time context) 1.0))
+    ]
+  )
+(defn chakra-3 []
+  [:div
+    (-> (ping 369)
+        (audio/connect-> audio/destination)
+        (audio/run-with context (audio/current-time context) 1.0))
+    ]
+  )
+(defn chakra-4 []
+  [:div
+    (-> (ping 417)
+        (audio/connect-> audio/destination)
+        (audio/run-with context (audio/current-time context) 1.0))
+    ]
+  )
+(defn chakra-5 []
+  [:div
+    (-> (ping 528)
+        (audio/connect-> audio/destination)
+        (audio/run-with context (audio/current-time context) 1.0))
+    ]
+  )
+(defn chakra-6 []
+  [:div
+    (-> (ping 639)
+        (audio/connect-> audio/destination)
+        (audio/run-with context (audio/current-time context) 1.0))
+    ]
+  )
+(defn chakra-7 []
+  [:div
+    (-> (ping 741)
+        (audio/connect-> audio/destination)
+        (audio/run-with context (audio/current-time context) 1.0))
+    ]
+  )
+(defn chakra-8 []
+  [:div
+    (-> (ping 852)
+        (audio/connect-> audio/destination)
+        (audio/run-with context (audio/current-time context) 1.0))
+    ]
+  )
+(defn chakra-9 []
+  [:div
+    (-> (ping 963)
+        (audio/connect-> audio/destination)
+        (audio/run-with context (audio/current-time context) 1.0))
+    ]
+  )
+
+(def chakras [{:chakra 1 :color "gray" :location "foot" :render [chakra-1]}
+              {:chakra 2 :color "gray" :location "placenta" :render [chakra-2]}
+              {:chakra 3 :color "red" :location "root" :render [chakra-3]}
+              {:chakra 4 :color "orange" :location "sacral" :render [chakra-4]}
+              {:chakra 5 :color "yellow" :location "solar plexus" :render [chakra-5]}
+              {:chakra 6 :color "green" :location "heart" :render [chakra-6]}
+              {:chakra 7 :color "blue" :location "throat" :render [chakra-7]}
+              {:chakra 8 :color "purple" :location "3rd eye" :render [chakra-8]}
+              {:chakra 9 :color "violet" :location "crown" :render [chakra-9]}])
+
+(defn meter []
+   [:div#meter
+     (doall
+       (for [level chakras] ^{:key (:chakra level )}
+         [:div.chakra
+          {:id (str "chakra-" (:chakra level)),
+           :style {:background-color (:color level)}
+           :class
+           (if (= (:chakra @app-state) (:chakra level)) "active")
+           :on-click #(set-level! level)}
+          [:span (:chakra level)]]))])
+
+(defn status []
+  [:div#status
+   [:canvas#analyzer]])
+
+(defn visualize []
+  [:div#visualize {:style {:background-color (:color @app-state)}}
+   [status]])
+
+(defn controls []
+  [:div#controls
+   [:input.btn {:type "button" :value "Play"
+                :on-click #(println "Playing")}]
+   [:input.btn {:type "button" :value "Stop"
+                :on-click #(println "Stopping")}]])
+
+(defn below-threshold? [level]
+  (<= (:chakra @app-state) (:chakra level)))
+
+(defn is-level? [level]
+  (= (:chakra @app-state) (:chakra level)))
+
+(defn render-levels []
+  ; (doall (map :render (filter below-threshold? chakras))))
+  (doall (map :render (filter is-level? chakras))))
+
+(defn main []
+  [:div#main 
+   [meter]
+   [controls]
+   (render-levels)
+   [:p (str @app-state) (.-currentTime context)]])
+
+(r/render-component [main] (. js/document (getElementById "app")))
+
+(defn on-js-reload [])
