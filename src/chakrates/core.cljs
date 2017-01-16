62b6f8af499d1098888bfbba9821c0227db77a4d
Kerby Ferris <kerbyferris@gmail.com>
2017-01-15 19:07:25 -0800

    styles

--- src/chakrates/core.cljs
+++ src/chakrates/core.cljs
@@ -7,11 +7,7 @@
 
 (defonce context (audio/audio-context))
 
-(defonce app-state (atom {:chakra 5, :color "blue"}))
-
-(defn set-level! [level]
-  (swap! app-state assoc :chakra (:chakra level))
-  (swap! app-state assoc :color (:color level)))
+(defonce app-state (atom {:chakra 5, :color "blue", :freq 528}))
 
 (defn ping [freq]
   (connect->
@@ -19,118 +15,58 @@
     (audio/percussive 0.3 1.6) ; Try varying the attack and decay.
     (audio/gain 0.4)))          ; Try a bigger gain.
 
-(defn chakra-1 [] 
+(defn chakra [freq] 
   [:div
-    (-> (ping 174)
+    (-> (ping freq)
         (audio/connect-> audio/destination)
-        (audio/run-with context (audio/current-time context) 1.0))
-   ]
-  )
-(defn chakra-2 []
-  [:div
-    (-> (ping 285)
-        (audio/connect-> audio/destination)
-        (audio/run-with context (audio/current-time context) 1.0))
-    ]
-  )
-(defn chakra-3 []
-  [:div
-    (-> (ping 369)
-        (audio/connect-> audio/destination)
-        (audio/run-with context (audio/current-time context) 1.0))
-    ]
-  )
-(defn chakra-4 []
-  [:div
-    (-> (ping 417)
-        (audio/connect-> audio/destination)
-        (audio/run-with context (audio/current-time context) 1.0))
-    ]
-  )
-(defn chakra-5 []
-  [:div
-    (-> (ping 528)
-        (audio/connect-> audio/destination)
-        (audio/run-with context (audio/current-time context) 1.0))
-    ]
-  )
-(defn chakra-6 []
-  [:div
-    (-> (ping 639)
-        (audio/connect-> audio/destination)
-        (audio/run-with context (audio/current-time context) 1.0))
-    ]
-  )
-(defn chakra-7 []
-  [:div
-    (-> (ping 741)
-        (audio/connect-> audio/destination)
-        (audio/run-with context (audio/current-time context) 1.0))
-    ]
-  )
-(defn chakra-8 []
-  [:div
-    (-> (ping 852)
-        (audio/connect-> audio/destination)
-        (audio/run-with context (audio/current-time context) 1.0))
-    ]
-  )
-(defn chakra-9 []
-  [:div
-    (-> (ping 963)
-        (audio/connect-> audio/destination)
-        (audio/run-with context (audio/current-time context) 1.0))
-    ]
-  )
+        (audio/run-with context (audio/current-time context) 1.0))])
 
-(def chakras [{:chakra 1 :frequency 174 :color "white" :location "foot" :render [chakra-1]}
-              {:chakra 2 :frequency 285 :color "#aaa" :location "placenta" :render [chakra-2]}
-              {:chakra 3 :frequency 369 :color "red" :location "root" :render [chakra-3]}
-              {:chakra 4 :frequency 417 :color "orange" :location "sacral" :render [chakra-4]}
-              {:chakra 5 :frequency 528 :color "yellow" :location "solar plexus" :render [chakra-5]}
-              {:chakra 6 :frequency 639 :color "green" :location "heart" :render [chakra-6]}
-              {:chakra 7 :frequency 741 :color "skyblue" :location "throat" :render [chakra-7]}
-              {:chakra 8 :frequency 852 :color "purple" :location "3rd eye" :render [chakra-8]}
-              {:chakra 9 :frequency 963 :color "violet" :location "crown" :render [chakra-9]}])
+(defn set-level! [chakra]
+  (swap! app-state assoc :chakra (:chakra chakra))
+  (swap! app-state assoc :color (:color chakra))
+  (swap! app-state assoc :freq (:freq chakra)))
 
-(defn meter []
-   [:div#meter
-     (doall
-       (for [level chakras] ^{:key (:chakra level )}
-         [:div.chakra
-          {:id (str "chakra-" (:chakra level)),
-           :style {:background-color (:color level)}
-           :class
-           (if (= (:chakra @app-state) (:chakra level)) "active")
-           :on-click #(set-level! level)}
-          [:span (:chakra level)]]))])
+(def chakras [{:chakra 9 :freq 963 :color "violet" :location "crown"}
+              {:chakra 8 :freq 852 :color "purple" :location "3rd eye"}
+              {:chakra 7 :freq 741 :color "skyblue" :location "throat"}
+              {:chakra 6 :freq 639 :color "green" :location "heart"}
+              {:chakra 5 :freq 528 :color "yellow" :location "solar plexus"}
+              {:chakra 4 :freq 417 :color "orange" :location "sacral"}
+              {:chakra 3 :freq 369 :color "red" :location "root"}
+              {:chakra 2 :freq 285 :color "#aaa" :location "placenta"}
+              {:chakra 1 :freq 174 :color "white" :location "foot"}])
+
+(defn display-chakras []
+  [:div#chakras
+   [:ul
+    (doall
+      (for [chakra chakras] ^{:key (:chakra chakra)}
+        [:li.chakra
+         {:id (str "chakra-" (:chakra chakra)),
+          :style {:background-color (:color chakra)}
+          :class
+          (if (= (:chakra @app-state) (:chakra chakra)) "active")
+          :on-click #(set-level! chakra)}
+         (:location chakra)]))]])
 
 (defn controls []
   [:div#controls
    [:input.btn {:type "button" :value "Play"
-                :on-click #(println "Playing")}]
+                :on-click #(chakra (:freq @app-state))}]
    [:input.btn {:type "button" :value "Stop"
                 :on-click #(println "Stopping")}]])
 
-(defn below-threshold? [level]
-  (<= (:chakra @app-state) (:chakra level)))
-
-(defn is-level? [level]
-  (= (:chakra @app-state) (:chakra level)))
-
-(defn render-levels []
-  ; (doall (map :render (filter below-threshold? chakras))))
-  (doall (map :render (filter is-level? chakras))))
+(defn meta-data []
+  [:div#meta-data
+   [:ul
+    [:li "STATE: "(str @app-state)]
+    [:li "CURRENT TIME: "(.-currentTime context)]]])
 
 (defn main []
   [:div#main 
-   [meter]
    [controls]
-   (render-levels)
-   [:div#meta-data
-    [:ul
-     [:li "STATE: "(str @app-state)]
-     [:li "CURRENT TIME: "(.-currentTime context)]]]
+   [display-chakras]
+   [meta-data]
    ])
 
 (r/render-component [main] (. js/document (getElementById "app")))
