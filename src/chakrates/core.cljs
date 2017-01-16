aad4cc081e0b03f1809ce699b61bf375923724c4
Kerby Ferris <kerbyferris@gmail.com>
2017-01-15 19:54:21 -0800

    continuous play

--- src/chakrates/core.cljs
+++ src/chakrates/core.cljs
@@ -7,24 +7,27 @@
 
 (defonce context (audio/audio-context))
 
-(defonce app-state (atom {:chakra 5, :color "blue", :freq 528}))
+(defonce app-state (atom {:chakra 5, :color "blue", :freq 528, :is-playing false}))
+
+(defn set-level! [chakra]
+  (swap! app-state assoc :chakra (:chakra chakra))
+  (swap! app-state assoc :color (:color chakra))
+  (swap! app-state assoc :freq (:freq chakra)))
+
+(defn toggle-play []
+  (swap! app-state assoc :is-playing (not (:is-playing @app-state))))
 
 (defn ping [freq]
   (connect->
-    (audio/sine freq)         ; Try a sawtooth wave.
-    (audio/percussive 0.3 1.6) ; Try varying the attack and decay.
-    (audio/gain 0.4)))          ; Try a bigger gain.
+    (audio/sine freq)
+    (audio/percussive 0.3 1.6)
+    (audio/gain 0.4)))
 
 (defn chakra [freq] 
   [:div
     (-> (ping freq)
         (audio/connect-> audio/destination)
         (audio/run-with context (audio/current-time context) 1.0))])
-
-(defn set-level! [chakra]
-  (swap! app-state assoc :chakra (:chakra chakra))
-  (swap! app-state assoc :color (:color chakra))
-  (swap! app-state assoc :freq (:freq chakra)))
 
 (def chakras [{:chakra 9 :freq 963 :color "violet" :location "crown"}
               {:chakra 8 :freq 852 :color "purple" :location "3rd eye"}
@@ -34,10 +37,10 @@
               {:chakra 4 :freq 417 :color "orange" :location "sacral"}
               {:chakra 3 :freq 369 :color "red" :location "root"}
               {:chakra 2 :freq 285 :color "#aaa" :location "placenta"}
-              {:chakra 1 :freq 174 :color "white" :location "foot"}])
+              {:chakra 1 :freq 174 :color "#eee" :location "foot"}])
 
 (defn display-chakras []
-  [:div#chakras
+  [:div#chakras {:style {:background-color (:color @app-state)}}
    [:ul
     (doall
       (for [chakra chakras] ^{:key (:chakra chakra)}
@@ -51,10 +54,9 @@
 
 (defn controls []
   [:div#controls
-   [:input.btn {:type "button" :value "Play"
-                :on-click #(chakra (:freq @app-state))}]
-   [:input.btn {:type "button" :value "Stop"
-                :on-click #(println "Stopping")}]])
+   [:input.btn {:type "button"
+                :value (if (:is-playing @app-state) "Stop" "Play")
+                :on-click #(toggle-play)}]])
 
 (defn meta-data []
   [:div#meta-data
@@ -67,6 +69,7 @@
    [controls]
    [display-chakras]
    [meta-data]
+   (chakra (:freq @app-state))
    ])
 
 (r/render-component [main] (. js/document (getElementById "app")))
