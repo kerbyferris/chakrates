9225fc44c7a2cd8a5ec6857dfc7b9f84f4835661
Kerby Ferris <kerbyferris@gmail.com>
2017-01-18 21:31:32 -0800

    async

--- src/chakrates/core.cljs
+++ src/chakrates/core.cljs
@@ -10,30 +10,29 @@
 
 (enable-console-print!)
 
+(def EVENTCHANNEL (chan))
+
 (defonce context (audio/audio-context))
 
-(defonce full-volume 0.4)
+(defonce chakras [{:number 9 :freq 963 :color "violet" :location "crown"}
+                  {:number 8 :freq 852 :color "mediumpurple" :location "3rd eye"}
+                  {:number 7 :freq 741 :color "skyblue" :location "throat"}
+                  {:number 6 :freq 639 :color "yellowgreen" :location "heart"}
+                  {:number 5 :freq 528 :color "gold" :location "solar plexus"}
+                  {:number 4 :freq 417 :color "orange" :location "sacral"}
+                  {:number 3 :freq 369 :color "indianred" :location "root"}
+                  {:number 2 :freq 285 :color "#aaa" :location "foot"}
+                  {:number 1 :freq 174 :color "#eee" :location "earthstar"}])
 
-(defonce app-state (atom {:chakra 5,
-                          :color "yellow",
-                          :freq 528,
-                          :location "heart"
-                          :volume full-volume
+(defonce app-state (atom {:chakra {:number 5,
+                                   :freq 528,
+                                   :color "yellowgreen",
+                                   :location "heart"},
                           :word "chakrates"
                           }))
 
-(defonce chakras [{:chakra 9 :freq 963 :color "violet" :location "crown"}
-                  {:chakra 8 :freq 852 :color "mediumpurple" :location "3rd eye"}
-                  {:chakra 7 :freq 741 :color "skyblue" :location "throat"}
-                  {:chakra 6 :freq 639 :color "yellowgreen" :location "heart"}
-                  {:chakra 5 :freq 528 :color "gold" :location "solar plexus"}
-                  {:chakra 4 :freq 417 :color "orange" :location "sacral"}
-                  {:chakra 3 :freq 369 :color "indianred" :location "root"}
-                  {:chakra 2 :freq 285 :color "#aaa" :location "foot"}
-                  {:chakra 1 :freq 174 :color "#eee" :location "earthstar"}])
-
 (defn get-chakra [number]
-  (first (filter #(= number (:chakra %)) chakras)))
+  (first (filter #(= number (:number %)) chakras)))
 
 (defonce alphanumeric-map
   (zipmap
@@ -52,32 +51,36 @@
       n (recur (add-digits n)))))
 
 (defn word-to-numerology [word]
-  (number-to-digit
-    (word-to-number word)))
+  (number-to-digit (word-to-number word)))
 
 (defn ping []
   (connect->
-    (add (sine (:freq @app-state))
-         (sine (+ (count (:word @app-state)) (:freq @app-state))))
+    (add (sine (:freq (:chakra @app-state)))
+         (sine (+ (count (:word @app-state)) (:freq (:chakra @app-state)))))
     (percussive 0.8 3)
     (low-pass 200)
-    (gain (:volume @app-state))))
+    (gain 0.4)))
 
-(defn set-word! [word]
-  (let [chakra (get-chakra (word-to-numerology word))]
-    (swap! app-state assoc :chakra (:chakra chakra))
-    (swap! app-state assoc :color (:color chakra))
-    (swap! app-state assoc :freq (:freq chakra))
-    (swap! app-state assoc :location (:location chakra)))
-  (swap! app-state assoc :word word)
-  (-> (ping)
-      (connect-> destination)
-      (run-with context (audio/current-time context) 3)))
+(def EVENTS
+  {:update-chakra (fn [{:keys [active-chakra]}]
+                    (swap! app-state assoc-in [:chakra] active-chakra)),
+   :update-word (fn [{:keys [active-word]}]
+                  (swap! app-state assoc :word active-word)
+                  (swap! app-state assoc-in [:chakra]
+                         (get-chakra (word-to-numerology active-word)))
+                  (-> (ping)
+                      (connect-> destination)
+                      (run-with context (audio/current-time context) 3)))})
+
+(go
+  (while true
+    (let [[event-name event-data] (<! EVENTCHANNEL)]
+      ((event-name EVENTS) event-data))))
 
 (defonce word-uri "http://setgetgo.com/randomword/get.php")
 
 (defn handler [response]
-   (set-word! (str response)))
+  (put! EVENTCHANNEL [:update-word {:active-word response}])) 
 
 (defn get-word []
   (GET word-uri {:response-format :text
@@ -85,21 +88,22 @@
                  :handler handler }))
 
 (defn display-chakras []
-  [:div#chakras {:style {:background-color (:color @app-state)}}
+  [:div#chakras {:style {:background-color (:color (:chakra @app-state))}}
    [:ul
     (doall
-      (for [chakra chakras] ^{:key (:chakra chakra)}
+      (for [chakra chakras] ^{:key (:number chakra)}
         [:li.chakra
-         {:id (str "chakra-" (:chakra chakra)),
+         {:id (str "chakra-" (:number chakra)),
           :style {:background-color (:color chakra)}
-          :class (if (= (:chakra @app-state) (:chakra chakra)) "active")}
+          :class (if (= (:chakra @app-state) (:number chakra)) "active")
+          :on-click (fn [event] (put! EVENTCHANNEL [:update-chakra {:active-chakra chakra}]))}
          (:location chakra)]))]])
 
 (defn controls []
   [:div#controls
    [:input.btn {:type "button"
                 :value "Get Word"
-                :on-click #(set-word! (get-word))}]])
+                :on-click #(get-word)}]])
 
 (defn meta-data []
   [:div#meta-data
@@ -111,10 +115,10 @@
    [controls]
    [:div#word
     [:p (:word @app-state)]
-     [:ul
-      [:li "chakra: " (:chakra @app-state)]
-      [:li "frequency: " (:freq @app-state)]
-      [:li "location: " (:location @app-state)]
+     [:ul {:style {:color (:color (:chakra @app-state))}}
+      [:li "chakra: " (:number (:chakra @app-state))]
+      [:li "frequency: " (:freq (:chakra @app-state))]
+      [:li "location: " (:location (:chakra @app-state))]
       ]]
    [display-chakras]])
 
