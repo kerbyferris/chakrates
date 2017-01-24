403c76567102ace500bbf045eaa51e347208fff0
Kerby Ferris <kerbyferris@gmail.com>
2017-01-23 21:30:13 -0800

    remove out dir

--- resources/public/js/compiled/out/chakrates/test_runner.cljs
+++
@@ -1,9 +0,0 @@
-(ns chakrates.test-runner
-  (:require [doo.runner :refer-macros [doo-tests]]
-            [chakrates.numerology-test]
-            [chakrates.util-test]))
-
-(enable-console-print!)
-
-(doo-tests 'chakrates.numerology-test
-           'chakrates.util-test)
--- resources/public/js/compiled/out/chakrates/util.cljs
+++
@@ -1,7 +0,0 @@
-(ns chakrates.util
-  (:require [chakrates.data :as data]))
-
-(enable-console-print!)
-
-(defn get-chakra-by-number [number]
-  (first (filter #(= number (:number %)) data/chakras)))
